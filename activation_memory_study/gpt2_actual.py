a405b93c959b425d092f6560aa37669fa43fd6b5
DandinPower <tomhot246@gmail.com>
2024-10-02 22:03:48 +0800

    backup: for activation study still going on

--- activation_memory_study/gpt2_actual.py
+++ activation_memory_study/gpt2_actual.py
@@ -1,6 +1,13 @@
-from transformers import GPT2Tokenizer, GPT2Model
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
-model = GPT2Model.from_pretrained('gpt2-xl')
-text = "Replace me by any text you'd like."
+from transformers import AutoModel, AutoTokenizer
+
+model_name = "meta-llama/Llama-3.2-1B"
+context_length = 1024
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
+print(tokenizer.bos_token)
+text = "<|begin_of_text|>" * context_length
 encoded_input = tokenizer(text, return_tensors='pt')
+print(encoded_input)
 output = model(**encoded_input)
